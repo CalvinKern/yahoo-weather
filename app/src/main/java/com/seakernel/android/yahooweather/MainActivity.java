a2ef3b4101c0fd5d8efa7326ade5b29dd6b84949
Calvin Kern <ckern@pixio.com>
2017-05-26 14:31:41 -0600

    Added connection to controller for forecast

--- app/src/main/java/com/seakernel/android/yahooweather/MainActivity.java
+++ app/src/main/java/com/seakernel/android/yahooweather/MainActivity.java
@@ -3,7 +3,8 @@ package com.seakernel.android.yahooweather;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.seakernel.android.yahooweather.model.YahooResponse;
@@ -14,17 +15,43 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements Callback<YahooResponse> {
 
+    private ForecastRecyclerAdapter mAdapter;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAdapter = new ForecastRecyclerAdapter(null);
+
+        final RecyclerView recycler = (RecyclerView) findViewById(R.id.main_recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mAdapter = null; // Clear reference to adapter
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         NetworkHelper.getWeather(this);
     }
 
     @Override
     public void onResponse(@NonNull final Call<YahooResponse> call, @NonNull final Response<YahooResponse> response) {
-        // TODO: Load weather views
+        // Load weather views
+        final YahooResponse yahooResponse = response.body();
+        if (yahooResponse != null) {
+            mAdapter.updateForecasts(yahooResponse.getQuery().getResult().getChannel().getItem().getForecasts());
+        } else {
+            mAdapter.updateForecasts(null);
+        }
+
     }
 
     @Override
