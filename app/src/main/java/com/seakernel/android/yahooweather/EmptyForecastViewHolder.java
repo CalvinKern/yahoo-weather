438402bc82a74f04ad642324b0a67be17b131887
Calvin Kern <ckern@pixio.com>
2017-05-26 15:17:19 -0600

    Added layout and class for empty forecast

---
+++ app/src/main/java/com/seakernel/android/yahooweather/EmptyForecastViewHolder.java
@@ -0,0 +1,16 @@
+package com.seakernel.android.yahooweather;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Calvin on 5/26/17.
+ * Copyright © 2017 SeaKernel. All rights reserved.
+ */
+
+public class EmptyForecastViewHolder extends RecyclerView.ViewHolder {
+
+    public EmptyForecastViewHolder(final View itemView) {
+        super(itemView);
+    }
+}
