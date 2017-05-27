f1d31888f312f55f33cf8cf64b4dc2db25d209c5
Calvin Kern <ckern@pixio.com>
2017-05-27 10:23:08 -0600

    Added about screen

---
+++ app/src/main/java/com/seakernel/android/yahooweather/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.seakernel.android.yahooweather;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Calvin on 5/27/17.
+ * Copyright © 2017 SeaKernel. All rights reserved.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
